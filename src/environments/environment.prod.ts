7d34eb5612a97fe651f7d90ff2f4727571ebbbd1
Alex <alexsan11@hotmail.com>
2019-12-14 23:20:55 -0300

    edit variable of environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,14 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyBxEQlwQCgaCcoSx6P1W9t4mLbqEAg2Lww",
-    authDomain: "poupeprev.firebaseapp.com",
-    databaseURL: "https://poupeprev.firebaseio.com",
-    projectId: "poupeprev",
-    storageBucket: "poupeprev.appspot.com",
-    messagingSenderId: "130400992080",
-    appId: "1:130400992080:web:2e2990f73933f78035ca55"
+    firebaseConfig : {
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
+    }
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyBxEQlwQCgaCcoSx6P1W9t4mLbqEAg2Lww",
-    authDomain: "poupeprev.firebaseapp.com",
-    databaseURL: "https://poupeprev.firebaseio.com",
-    projectId: "poupeprev",
-    storageBucket: "poupeprev.appspot.com",
-    messagingSenderId: "130400992080",
-    appId: "1:130400992080:web:2e2990f73933f78035ca55"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
 
